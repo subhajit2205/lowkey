e3bf2a29d9649be3ca2448cc6cc63b0a672bb310
maedi <maediprichard@gmail.com>
2026-02-09 21:15:57 +1100

    Merge AST with lowkey

--- lib/lowkey.rb
+++ lib/lowkey.rb
@@ -1,14 +1,33 @@
 # frozen_string_literal: true
 
 require 'prism'
-require_relative 'ast'
+
+require_relative 'visitors/visitor'
+require_relative 'proxies/file_proxy'
 
 module Lowkey
   class << self
-    def parse(file_path)
+    def files
+      @files ||= {}
+    end
+
+    def parse(file_path:)
       root_node = Prism.parse_file(file_path).value
-      klass = Object # TODO.
-      AST.new(root_node:, klass:, file_path:)
+      file_proxy = FileProxy.new(path: file_path, root_node:)
+
+      parent_map = ParentMap.new(root_node:)
+      visitor = Visitor.new(file_proxy:, parent_map:)
+      root_node.accept(visitor)
+
+      files[file_path] = file_proxy
+    end
+
+    def [](file_path)
+      files[file_path]
+    end
+
+    def clear
+      files = {}
     end
   end
 end
